cde1c3c2b40ba7c712368b734aed9c342005dd30
Michael Filonenko <filonenko.mikhail@gmail.com>
2018-05-29 16:09:27 +0300

    tarantool: added linux and bsd library searching

--- lib/icu-date/detect_icu_version_suffix.lua
+++ lib/icu-date/detect_icu_version_suffix.lua
@@ -5,6 +5,15 @@ if ffi.os == 'OSX' then
     uint32_t _dyld_image_count();
     const char* _dyld_get_image_name(uint32_t image_index);
   ]]
+elseif ffi.os == 'Linux' or ffi.os == 'BSD' then
+  ffi.cdef[[
+    struct dl_phdr_info {
+      void*             dlpi_addr;  /* Base address of object */
+      const char       *dlpi_name;  /* (Null-terminated) name of object */
+    };
+    typedef int (*enum_callback) (struct dl_phdr_info *info, size_t size, void *data);
+    int dl_iterate_phdr(enum_callback callback, void *data);
+  ]]
 end
 
 
@@ -24,16 +33,39 @@ return function()
     return detected_suffix
   end
 
-  for i = 0, ffi.C._dyld_image_count()-1 do
-      local path = ffi.C._dyld_get_image_name(i)
+  if ffi.os == 'OSX' then
+      for i = 0, ffi.C._dyld_image_count()-1 do
+          local path = ffi.C._dyld_get_image_name(i)
 
-      if path ~= nil then
-          local _, _, prefix = ffi.string(path):find('^.*libicui18n%.(%d%d)%.dylib')
-          if prefix ~= nil then
-              return '_'..prefix, ffi.string(path)
+          if path ~= nil then
+              local _, _, prefix = ffi.string(path):find('^.*libicui18n%.(%d%d)%.dylib')
+              if prefix ~= nil then
+                  return '_'..prefix, ffi.string(path)
+              end
           end
       end
+  elseif ffi.os == 'Linux' or ffi.os == 'BSD' then
+      local found, found_path
+      local cb = ffi.cast('enum_callback', function(info, size, data)
+                              if info ~= nil and info['dlpi_name'] ~= nil then
+                                  local path = ffi.string(info['dlpi_name'])
+                                  local _, _, prefix = path:find('^.*libicui18n%.so%.(%d%d)')
+                                  if prefix ~= nil then
+                                      found = '_'..prefix
+                                      found_path = path
+                                      return 1
+                                  end
+                              end
+                              return 0
+      end)
+      local _ = ffi.C.dl_iterate_phdr(cb, nil)
+      cb:free()
+
+      if found ~= nil and found_path ~= nil then
+          return found, found_path
+      end
   end
+
 
   -- Some systems, like OS X don't have a suffix.
   local possible_suffixes = {""}
